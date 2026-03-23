439634b76234c3f4ac90e437be3681f16c1ed38d
wcmyk <weiceo@weiverse.org>
2026-03-23 15:07:22 -0400

    Add CircuitApp component wrapper implementation

---
+++ src/apps/CircuitApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CircuitApp: React.FC = () => {
+    return (
+        <div>
+            <h1>Circuit Application</h1>
+            {/* Additional components can be added here */}
+        </div>
+    );
+};
+
+export default CircuitApp;
